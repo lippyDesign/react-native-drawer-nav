6bdd7fe7864e1fa660117a46e9c2a420fc0d01d4
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-12-27 17:50:37 -0800

    added common components

---
+++ simple-notepad/src/components/common/index.js
@@ -0,0 +1,8 @@
+// in a single line we can import and immediately export using the following syntax
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+export * from './Confirm';
